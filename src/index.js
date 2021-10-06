518bfcf95d3a7744e91155fb7b1aedc4710cdfa7
mdidius <morphydidius@gmail.com>
2021-10-06 23:46:01 +0300

    login page start

--- src/index.js
+++ src/index.js
@@ -1,11 +1,15 @@
 import './styles/main.scss';
-import Handlebars from 'handlebars';
+import Handlebars from 'handlebars/runtime';
+import login from './pages/login/index';
 
 // import { print } from './modules/sum';
 
 const root = document.createElement('div');
-const template = Handlebars.compile("Names: {{name}}");
+// const template = Handlebars.compile("Names: {{name}}");
+const template = Handlebars.compile(login);
 
-root.textContent = template({ name: "Nils" });
+// root.textContent = template({ name: "Nils" });
+root.innerHTML = template();
+// root.innerHTML = '<div>mytest</div>';
 
-document.body.append(root);
+// document.body.append(root);
