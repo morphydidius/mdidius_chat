83eaf3b95eb37ad48fc41cc75649ced57c79f610
mdidius <morphydidius@gmail.com>
2021-10-08 00:22:59 +0300

    login page with styles

--- handlebars.config.js
+++ handlebars.config.js
@@ -1,7 +1,7 @@
 module.exports = {
-  pages: 'pages',
-  // data: 'views/json',
-  // helpers: 'views/tools',
-  layouts: 'src/layout',
-  partials: 'src/partials',
+	pages: 'pages',
+	// data: 'views/json',
+	// helpers: 'views/tools',
+	layouts: 'src/layout',
+	partials: 'src/partials',
 };
